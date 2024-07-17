278fbae3ff62e3675a35ceddde2f704cbde4377d
Bryan Leonardo Calix <bcalixvelasquez@gmail.com>
2024-07-16 20:32:47 -0600

    Correccion en queries

--- controllers/graficos.controller.js
+++ controllers/graficos.controller.js
@@ -1,5 +1,5 @@
 const { request, response } = require("express");
-const { NumeroFamAtendidas } = require("../models");
+const { NumeroFamAtendidas, Monitoreo } = require("../models");
 const { sequelize, QueryTypes } = require("sequelize");
 const { getResponse } = require("../helpers/getResponde");
 const models = require('../models');
@@ -9,10 +9,13 @@ const porcentajeFamilasAtendidas = async (req = request, res = response) => {
 
         let sequelize = await models.sequelize;
 
+        const monitoreo = await Monitoreo.findByPk(req.body.monitoreoId);
+
         const query = `SELECT CAST(((ejecutadas/programadas) * 100) AS DECIMAL(10,2)) as PEjecutadas, (100-CAST(((ejecutadas/programadas) * 100) AS DECIMAL(10,2))) as PNoEjecutadas, * from NumeroFamAtendidas ` +
-            `WHERE monitoreoId = ${req.body.monitoreoId} ` +
+            `WHERE monitoreoId = ${monitoreo.CommunityId} ` +
             `and CAST(createdAt as date) > CAST('${req.body.fechaInicial}' as date) ` +
-            `and YEAR(CAST('${req.body.fechaFinal}' as date) ) = ${req.body.anio} `;
+            `and CAST(createdAt as date) < CAST('${req.body.fechaFinal}' as date) ` +
+            `and YEAR(CAST(createdAt as date) ) = ${req.body.anio} `;
 
         console.log(query)
 
@@ -33,10 +36,12 @@ const porcentajeNiñosDesnutricion = async (req = request, res = response) => {
 
         let sequelize = await models.sequelize;
 
-        const query = `        SELECT noDesnutridos, desnutridos, (noDesnutridos + desnutridos) as total, (noDesnutridos/(noDesnutridos + desnutridos)) as pnoDesnutridos, (Desnutridos/(noDesnutridos + desnutridos)) pdesnutridos, createdAt from ninoDesnutricions  ` +
+
+        const query = `SELECT noDesnutridos, desnutridos, (noDesnutridos + desnutridos) as total, (noDesnutridos/(noDesnutridos + desnutridos)) as pnoDesnutridos, (Desnutridos/(noDesnutridos + desnutridos)) pdesnutridos, createdAt from ninoDesnutricions  ` +
             `WHERE monitoreoId = ${req.body.monitoreoId} ` +
             `and CAST(createdAt as date) > CAST('${req.body.fechaInicial}' as date) ` +
-            `and YEAR(CAST('${req.body.fechaFinal}' as date) ) = ${req.body.anio} `;
+            `and CAST(createdAt as date) < CAST(${req.body.fechaFinal} as date) ` +
+            `and YEAR(CAST('createdAt' as date) ) = ${req.body.anio} `;
 
         console.log(query)
 
